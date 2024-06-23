689dd3c9e01fd3f4e30a507732a6f71f0435a58b
jschuetter <jmbschuetter@gmail.com>
2024-06-23 13:09:46 -0400

    Updated config.py
    Updated config.py to include more options
    
    - Updated default-env to match
    
    Fixed bug in config.py
    
    Added def. values to config.py
    
    Added def. values to config.py

--- display-master/config.py
+++ display-master/config.py
@@ -10,31 +10,34 @@ def matrix_from_env():
     load_dotenv()
     options = RGBMatrixOptions()
 
-    options.hardware_mapping = os.getenv("GPIO_MAPPING")
-    options.panel_type = os.getenv("LED_PANEL_TYPE")
-    options.rows = int(os.getenv("MATRIX_ROWS"))
-    options.cols = int(os.getenv("MATRIX_COLS"))
-    options.chain_length = int(os.getenv("CHAIN_LENGTH"))
-    options.parallel = int(os.getenv("PARALLEL"))
-    options.multiplexing = int(os.getenv("MUX"))
-    options.pixel_mapper_config = os.getenv("PX_MAP")
-    options.pwm_bits = int(os.getenv("PWM_BITS"))
-    # Scan mode? - os.getenv("SCAN_MODE")
-    options.row_address_type =  int(os.getenv("ADDR_TYPE"))
-    # Invert display? - os.getenv("INVERT_COLORS")
-    options.led_rgb_sequence = os.getenv("RGB_SEQ")
-    options.pwm_lsb_nanoseconds = int(os.getenv("PWM_LSB_NS"))
-    # Dithering? - os.getenv("PWM_DITHER_BITS")
-    options.show_refresh_rate = int(os.getenv("SHOW_REFRESH"))
-    # Refresh limit?
-    # Brightness?
+    options.hardware_mapping = os.getenv("GPIO_MAPPING", "regular")
+    options.panel_type = os.getenv("LED_PANEL_TYPE", "")
+    options.rows = int(os.getenv("MATRIX_ROWS", 32))
+    options.cols = int(os.getenv("MATRIX_COLS", 32))
+    options.chain_length = int(os.getenv("CHAIN_LENGTH", 1))
+    options.parallel = int(os.getenv("PARALLEL", 1))
+    options.multiplexing = int(os.getenv("MUX", 0))
+    options.pixel_mapper_config = os.getenv("PX_MAP", "")
+    options.pwm_bits = int(os.getenv("PWM_BITS", 11))
+    options.scan_mode = int(os.getenv("SCAN_MODE", 0))
+    options.row_address_type =  int(os.getenv("ADDR_TYPE", 0))
+    options.inverse_colors = int(os.getenv("INVERT_COLORS", 0))
+    options.led_rgb_sequence = os.getenv("RGB_SEQ", "RGB")
+    options.pwm_lsb_nanoseconds = int(os.getenv("PWM_LSB_NS", 130))
+    options.pwm_dither_bits = int(os.getenv("PWM_DITHER_BITS", 0))
+    options.show_refresh_rate = int(os.getenv("SHOW_REFRESH", 0))
+    options.limit_refresh_rate_hz = int(os.getenv("LIMIT_REFRESH", 0))
+    options.brightness = int(os.getenv("BRIGHTNESS", 100))
     if int(os.getenv("NO_HW_PULSE")):
         options.disable_hardware_pulsing = True
+    # Runtime options
     if os.getenv("SLOWDOWN_GPIO", None) != None:
         options.gpio_slowdown = int(os.getenv("SLOWDOWN_GPIO"))
+    if int(os.getenv("DAEMON")): 
+        options.daemon = 1
     if not int(os.getenv("NO_DROP_PRIVS")):
         options.drop_privileges=False
-    # Daemon?
+    
     # Drop priv UID/GID?
 
     return RGBMatrix(options = options)
